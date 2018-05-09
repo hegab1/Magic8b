af2c47c389615c040ba2997fc1e2186ad495f897
Mohamed A.Hegab <mohameda.hegab@Mohameds-Mac.local>
2018-05-09 06:56:49 -0700

    Initial Commit

---
+++ magic 8 ball/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  magic 8 ball
+//
+//  Created by Mohamed A.Hegab on 5/9/18.
+//  Copyright © 2018 Mohamed A.Hegab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
